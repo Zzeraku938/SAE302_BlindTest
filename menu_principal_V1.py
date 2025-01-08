b31245329ee0b804dbb0b331b08a38b882808458
Slifer93 <yaco1601@gmail.com>
2025-01-08 23:05:24 +0100

    selection des genres

--- menu_principal_V1.py
+++ menu_principal_V1.py
@@ -37,3 +37,29 @@ class BlindtestInterface:
         self.selected_genre = None
         self.current_song = None
         self.score = 0
+
+
+        self.genre_label = tk.Label(self.master, text="Choisissez un genre musical :", font=("Arial", 18))
+        self.genre_label.pack(pady=20)
+
+        self.genre_var = tk.StringVar(value="RAP FR")
+        for genre in self.genres.keys():
+            rb = tk.Radiobutton(self.master, text=genre, variable=self.genre_var, value=genre, command=self.select_genre)
+            rb.pack(anchor=tk.W)
+
+        self.play_button = tk.Button(self.master, text="Jouer l'extrait", command=self.play_music)
+        self.play_button.pack(pady=10)
+
+        self.answer_entry = tk.Entry(self.master, width=50)
+        self.answer_entry.pack(pady=10)
+
+        self.submit_button = tk.Button(self.master, text="Valider", command=self.check_answer)
+        self.submit_button.pack(pady=10)
+
+        self.score_label = tk.Label(self.master, text="Score : 0", font=("Arial", 16))
+        self.score_label.pack(pady=20)
+    
+    def select_genre(self):
+        genre_name = self.genre_var.get()
+        self.current_song = random.choice(self.genres[genre_name])
+        print(f"Genre sélectionné : {genre_name}. Chanson actuelle : {self.current_song['title']}")
