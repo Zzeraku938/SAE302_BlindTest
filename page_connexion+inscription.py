ecc9097f1e32ce7f359be190af6ad33528acdb2e
VYMQuizz <VYMQuizz@gmail.com>
2025-01-09 12:25:32 +0100

    inscription utilisateurs + hash

--- page_connexion+inscription.py
+++ page_connexion+inscription.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hashlib
 
 # Initialisation de la base de données
 conn = sqlite3.connect('blindtest_users.db')
@@ -15,3 +16,18 @@ CREATE TABLE IF NOT EXISTS utilisateurs (
 )
 ''')
 conn.commit()
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def inscrire_utilisateur(nom, prenom, nom_utilisateur, email, mot_de_passe):
+    try:
+        hashed_password = hash_password(mot_de_passe)
+        cursor.execute('''
+        INSERT INTO utilisateurs (nom, prenom, nom_utilisateur, email, mot_de_passe)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (nom, prenom, nom_utilisateur, email, hashed_password))
+        conn.commit()
+        return True
+    except sqlite3.IntegrityError:
+        return False
